3adc8deea473dade5ea806f6a84cecfe54709e1a
Andrey Atapin <a.atapin@gmail.com>
2021-05-28 13:35:27 +0500

    review - simplified item repo

--- scanner/src/main/kotlin/com/rarible/flow/scanner/repo/ItemRepository.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/repo/ItemRepository.kt
@@ -3,7 +3,10 @@ package com.rarible.flow.scanner.repo
 import com.rarible.flow.scanner.model.Item
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
 
 
 @Repository
-interface  ItemRepository: ReactiveMongoRepository<Item, String>, ItemRepositoryCustom
+interface  ItemRepository: ReactiveMongoRepository<Item, String> {
+    fun findTopByOrderByUpdateTimeDesc(): Mono<Item>
+}
