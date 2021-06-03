6f38bb6dfd8528635300b9d937985f36491663c1
Andrey Atapin <a.atapin@gmail.com>
2021-06-03 14:39:57 +0500

    Reorganized build to subprojects

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -1,22 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.0"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.0"
-    kotlin("plugin.spring") version "1.5.0"
-}
-
-group = "com.rarible.flow"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
-
-repositories {
-    mavenCentral()
-
-    maven {
-        url = uri("https://jitpack.io")
-    }
+    kotlin("jvm")
+    kotlin("plugin.spring")
 }
 
 dependencies {
@@ -25,35 +10,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
     implementation("io.grpc:grpc-stub:1.37.0")
     implementation("io.grpc:grpc-core:1.37.0")
     implementation("io.grpc:grpc-protobuf:1.37.0")
     implementation("org.onflow:flow-jvm-sdk:0.1.1")
     implementation("net.devh:grpc-client-spring-boot-starter:2.12.0.RELEASE")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.register("stage") {
-    dependsOn(tasks["build"])
-}
-
-tasks.named("build") {
-    mustRunAfter(tasks["clean"])
 }
