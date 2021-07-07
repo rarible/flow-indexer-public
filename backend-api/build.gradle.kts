921d4cd64fc9d48b2127f9673b2358b8c294a6d7
Andrey Atapin <a.atapin@gmail.com>
2021-07-07 13:46:42 +0500

    FB-58 reorganized openapi generation

--- backend-api/build.gradle.kts
+++ backend-api/build.gradle.kts
@@ -1,10 +1,7 @@
-import kotlin.collections.mapOf
-
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
     id("org.springframework.boot")
-    id("org.openapi.generator")
 }
 
 dependencies {
@@ -13,27 +10,11 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation(rootProject.libs.bundles.rarible.core)
     implementation(rootProject.libs.rarible.core.starter)
+    implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.5.9")
 }
 
 tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
     enabled = true
     destinationDirectory.set(file("./target/boot"))
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    dependsOn("openApiGenerate")
-}
-
-openApiGenerate {
-    generatorName.set("kotlin-spring")
-    inputSpec.set("$rootDir/spec/nft-api.yaml")
-    outputDir.set("$buildDir/generated")
-    apiPackage.set("com.rarible.flow.api")
-    invokerPackage.set("com.rarible.flow.invoker")
-    modelPackage.set("com.rarible.flow.api.model")
-    configOptions.set(
-        mapOf(
-            "dateLibrary" to "java8"
-        )
-    )
 }
