ffc023f2dc94374a9bf8fe9d48f77d412ee9d98e
Andrey Atapin <a.atapin@gmail.com>
2021-07-14 11:24:40 +0300

    compilation fix

--- backend-api/build.gradle.kts
+++ backend-api/build.gradle.kts
@@ -23,4 +23,5 @@ dependencies {
 tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
     enabled = true
     destinationDirectory.set(file("./target/boot"))
+    duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.EXCLUDE
 }
