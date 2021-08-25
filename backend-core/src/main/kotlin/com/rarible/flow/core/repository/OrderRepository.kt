a2bbb0bf3584fecbfffe4f2e8d96312f6f276e6a
Andrey Atapin <a.atapin@gmail.com>
2021-08-25 14:24:17 +0500

    FB-215 aggregation apis

--- backend-core/src/main/kotlin/com/rarible/flow/core/repository/OrderRepository.kt
+++ backend-core/src/main/kotlin/com/rarible/flow/core/repository/OrderRepository.kt
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.Instant
 import java.time.LocalDateTime
 
 interface OrderRepository: ReactiveMongoRepository<Order, Long> {
