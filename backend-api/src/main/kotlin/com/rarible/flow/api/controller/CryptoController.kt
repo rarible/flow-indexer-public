0e56b27cc61df767b29f55f43d2b6ad3d1455361
Andrey Atapin <a.atapin@gmail.com>
2021-09-24 15:07:42 +0500

    review fixes

---
+++ backend-api/src/main/kotlin/com/rarible/flow/api/controller/CryptoController.kt
@@ -0,0 +1,23 @@
+package com.rarible.flow.api.controller
+
+import com.rarible.flow.api.service.FlowSignatureService
+import com.rarible.protocol.flow.nft.api.controller.FlowNftCryptoControllerApi
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RestController
+
+@CrossOrigin
+@RestController
+class CryptoController(
+    private val signatureService: FlowSignatureService
+): FlowNftCryptoControllerApi {
+    override suspend fun verifySignature(
+        publicKey: String,
+        signature: String,
+        message: String
+    ): ResponseEntity<Boolean> {
+        return signatureService.verify(
+            publicKey, signature, message
+        ).okOr404IfNull()
+    }
+}
--- backend-api/src/main/kotlin/com/rarible/flow/api/service/FlowSignatureService.kt
+++ backend-api/src/main/kotlin/com/rarible/flow/api/service/FlowSignatureService.kt
@@ -53,40 +53,7 @@ class FlowSignatureService(
         return scriptResult.value ?: false
     }
 
-    val scriptCode = """
-        import Crypto
+    private val scriptCode = this.javaClass.getResource("/script/sig_verify.cdc") .readText()
 
-        pub fun main(rawPublicKeys: [String], weights: [UFix64], signatures: [String], signedData: String): Bool {
-          let keyList = Crypto.KeyList()
-          var i = 0
-          for rawPublicKey in rawPublicKeys {
-            keyList.add(
-              PublicKey(
-                publicKey: rawPublicKey.decodeHex(),
-                signatureAlgorithm: SignatureAlgorithm.ECDSA_secp256k1 //SignatureAlgorithm.ECDSA_P256  
-              ),
-              hashAlgorithm: HashAlgorithm.SHA3_256,
-              weight: weights[i],
-            )
-            i = i + 1
-          }
 
-          let signatureSet: [Crypto.KeyListSignature] = []
-          var j = 0
-          for signature in signatures {
-            signatureSet.append(
-              Crypto.KeyListSignature(
-                keyIndex: j,
-                signature: signature.decodeHex()
-              )
-            )
-            j = j + 1
-          }
-
-          return keyList.verify(
-            signatureSet: signatureSet,
-            signedData: signedData.decodeHex(),
-          )
-        }
-    """.trimIndent()
 }
