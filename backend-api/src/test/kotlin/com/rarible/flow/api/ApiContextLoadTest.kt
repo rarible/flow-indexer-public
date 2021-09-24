edb1dde19107b4b6f24f444249db2016f0c4ed73
Andrey Atapin <a.atapin@gmail.com>
2021-09-24 17:03:08 +0500

    Context load test fix

--- backend-api/src/test/kotlin/com/rarible/flow/api/ApiContextLoadTest.kt
+++ backend-api/src/test/kotlin/com/rarible/flow/api/ApiContextLoadTest.kt
@@ -1,9 +1,13 @@
 package com.rarible.flow.api
 
+import com.rarible.flow.api.config.Config
+import com.rarible.flow.core.config.CoreConfig
 import org.junit.jupiter.api.Test
+import org.springframework.context.annotation.Import
 
 
 @IntegrationTest
+@Import(Config::class, CoreConfig::class)
 class ApiContextLoadTest: BaseIntegrationTest() {
 
     @Test
