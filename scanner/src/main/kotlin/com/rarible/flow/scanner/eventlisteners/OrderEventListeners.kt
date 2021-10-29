d38d9c7687d5beea4cfdded2ad4eb577dd6be35e
Andrey Atapin <a.atapin@gmail.com>
2021-10-29 12:35:06 +0500

    FB-307 refactored order subscriber

--- scanner/src/main/kotlin/com/rarible/flow/scanner/eventlisteners/OrderEventListeners.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/eventlisteners/OrderEventListeners.kt
@@ -1,23 +1,16 @@
 package com.rarible.flow.scanner.eventlisteners
 
-import com.nftco.flow.sdk.FlowAddress
-import com.rarible.flow.core.domain.*
-import com.rarible.flow.core.repository.OrderRepository
-import com.rarible.flow.core.repository.coFindById
-import com.rarible.flow.core.repository.coSave
+import com.rarible.flow.core.domain.FlowNftOrderActivityCancelList
+import com.rarible.flow.core.domain.FlowNftOrderActivityList
+import com.rarible.flow.core.domain.FlowNftOrderActivitySell
 import com.rarible.flow.scanner.ProtocolEventPublisher
 import com.rarible.flow.scanner.service.OrderService
 import kotlinx.coroutines.runBlocking
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
-import java.math.BigDecimal
-import java.math.BigInteger
-import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 @Component
 class OrderEventListeners(
-    private val orderRepository: OrderRepository,
     private val protocolEventPublisher: ProtocolEventPublisher,
     private val orderService: OrderService
 ) {
@@ -31,55 +24,15 @@ class OrderEventListeners(
 
     @EventListener(FlowNftOrderActivitySell::class)
     fun orderClose(activity: FlowNftOrderActivitySell) = runBlocking {
-        val order = orderRepository.coFindById(activity.hash.toLong())?.copy(
-            fill = BigDecimal.ONE,
-            makeStock = BigInteger.ZERO,
-            taker = FlowAddress(activity.right.maker),
-            status = OrderStatus.FILLED,
-            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
-        ) ?: Order(
-            id = activity.hash.toLong(),
-            fill = BigDecimal.ONE,
-            makeStock = BigInteger.ZERO,
-            taker = FlowAddress(activity.right.maker),
-            status = OrderStatus.FILLED,
-
-            maker = FlowAddress(activity.left.maker),
-            itemId = ItemId(activity.left.asset.contract, activity.tokenId),
-            amount = activity.price,
-            collection = activity.contract,
-            make = FlowAssetEmpty,
-            take = FlowAssetEmpty,
-            data = OrderData(emptyList(), emptyList()),
-            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
+        protocolEventPublisher.onUpdate(
+            orderService.close(activity)
         )
-
-        protocolEventPublisher.onUpdate(orderRepository.coSave(order))
     }
 
     @EventListener(FlowNftOrderActivityCancelList::class)
     fun orderCancelled(activity: FlowNftOrderActivityCancelList) = runBlocking {
-
-        val order = orderRepository.coFindById(activity.hash.toLong())?.copy(
-            cancelled = true,
-            status = OrderStatus.CANCELLED,
-            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
-        ) ?: Order(
-            id = activity.hash.toLong(),
-            cancelled = true,
-            status = OrderStatus.CANCELLED,
-
-            itemId = ItemId(activity.make.contract, activity.tokenId),
-            amount = activity.price,
-            collection = activity.contract,
-            maker = FlowAddress(activity.maker),
-            make = activity.make,
-            take = activity.take,
-            data = OrderData(emptyList(), emptyList()),
-            makeStock = activity.make.value.toBigInteger(),
-            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
+        protocolEventPublisher.onUpdate(
+            orderService.cancel(activity)
         )
-
-        protocolEventPublisher.onUpdate(orderRepository.coSave(order))
     }
 }
--- scanner/src/main/kotlin/com/rarible/flow/scanner/service/OrderService.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/service/OrderService.kt
@@ -1,7 +1,10 @@
 package com.rarible.flow.scanner.service
 
 import com.nftco.flow.sdk.FlowAddress
+import com.rarible.flow.core.domain.FlowAssetEmpty
+import com.rarible.flow.core.domain.FlowNftOrderActivityCancelList
 import com.rarible.flow.core.domain.FlowNftOrderActivityList
+import com.rarible.flow.core.domain.FlowNftOrderActivitySell
 import com.rarible.flow.core.domain.ItemId
 import com.rarible.flow.core.domain.Order
 import com.rarible.flow.core.domain.OrderData
@@ -11,7 +14,10 @@ import com.rarible.flow.core.domain.Payout
 import com.rarible.flow.core.repository.OrderRepository
 import com.rarible.flow.core.repository.coFindById
 import com.rarible.flow.core.repository.coSave
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
+import java.math.BigInteger
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
@@ -57,4 +63,56 @@ class OrderService(
         return orderRepository.coSave(order)
     }
 
+    //TODO tests
+    suspend fun close(activity: FlowNftOrderActivitySell): Order {
+        val order = orderRepository.coFindById(activity.hash.toLong())?.copy(
+            fill = BigDecimal.ONE,
+            makeStock = BigInteger.ZERO,
+            taker = FlowAddress(activity.right.maker),
+            status = OrderStatus.FILLED,
+            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
+        ) ?: Order(
+            id = activity.hash.toLong(),
+            fill = BigDecimal.ONE,
+            makeStock = BigInteger.ZERO,
+            taker = FlowAddress(activity.right.maker),
+            status = OrderStatus.FILLED,
+
+            maker = FlowAddress(activity.left.maker),
+            itemId = ItemId(activity.left.asset.contract, activity.tokenId),
+            amount = activity.price,
+            collection = activity.contract,
+            make = FlowAssetEmpty,
+            take = FlowAssetEmpty,
+            data = OrderData(emptyList(), emptyList()),
+            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
+        )
+
+        return orderRepository.coSave(order)
+    }
+
+    //TODO tests
+    suspend fun cancel(activity: FlowNftOrderActivityCancelList): Order {
+        val order = orderRepository.coFindById(activity.hash.toLong())?.copy(
+            cancelled = true,
+            status = OrderStatus.CANCELLED,
+            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
+        ) ?: Order(
+            id = activity.hash.toLong(),
+            cancelled = true,
+            status = OrderStatus.CANCELLED,
+
+            itemId = ItemId(activity.make.contract, activity.tokenId),
+            amount = activity.price,
+            collection = activity.contract,
+            maker = FlowAddress(activity.maker),
+            make = activity.make,
+            take = activity.take,
+            data = OrderData(emptyList(), emptyList()),
+            makeStock = activity.make.value.toBigInteger(),
+            lastUpdatedAt = LocalDateTime.ofInstant(activity.timestamp, ZoneOffset.UTC),
+        )
+
+        return orderRepository.coSave(order)
+    }
 }
