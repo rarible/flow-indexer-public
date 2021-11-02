0935a90ced2eebda8259d250df0427d2f15f6060
zolt <timochkinea@gmail.com>
2021-11-02 16:48:02 +0700

    FB-379 skip kafka message publishing for REINDEX block's

--- scanner/src/main/kotlin/com/rarible/flow/scanner/eventlisteners/OrderEventListeners.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/eventlisteners/OrderEventListeners.kt
@@ -1,9 +1,11 @@
 package com.rarible.flow.scanner.eventlisteners
 
+import com.rarible.blockchain.scanner.framework.data.Source
 import com.rarible.flow.core.domain.FlowNftOrderActivityCancelList
 import com.rarible.flow.core.domain.FlowNftOrderActivityList
 import com.rarible.flow.core.domain.FlowNftOrderActivitySell
 import com.rarible.flow.core.kafka.ProtocolEventPublisher
+import com.rarible.flow.scanner.model.IndexerEvent
 import com.rarible.flow.scanner.service.OrderService
 import kotlinx.coroutines.runBlocking
 import org.springframework.context.event.EventListener
@@ -16,44 +18,52 @@ class OrderEventListeners(
     private val orderService: OrderService,
     private val protocolEventPublisher: ProtocolEventPublisher,
 
-) {
+    ) {
 
-    @EventListener(FlowNftOrderActivityList::class)
-    fun list(activity: FlowNftOrderActivityList): Unit = runBlocking {
-        protocolEventPublisher.onUpdate(
-            orderService.list(activity)
-        )
+    @EventListener(condition = "#event.activity instanceof T(com.rarible.flow.core.domain.FlowNftOrderActivityList)")
+    fun list(event: IndexerEvent): Unit = runBlocking {
+        val activity = event.activity as FlowNftOrderActivityList
+        val o = orderService.list(activity)
+        if (event.source != Source.REINDEX) {
+            protocolEventPublisher.onUpdate(o)
+        }
     }
 
-    @EventListener(FlowNftOrderActivitySell::class)
-    fun orderClose(activity: FlowNftOrderActivitySell): Unit = runBlocking {
-        protocolEventPublisher.onUpdate(
-            orderService.close(activity)
-        )
+    @EventListener(condition = "#event.activity instanceof T(com.rarible.flow.core.domain.FlowNftOrderActivitySell)")
+    fun orderClose(event: IndexerEvent): Unit = runBlocking {
+        val activity = event.activity as FlowNftOrderActivitySell
+        val o = orderService.close(activity)
+        if (event.source != Source.REINDEX) {
+            protocolEventPublisher.onUpdate(o)
+        }
     }
 
-    @EventListener(FlowNftOrderActivityCancelList::class)
-    fun orderCancelled(activity: FlowNftOrderActivityCancelList): Unit = runBlocking {
-        protocolEventPublisher.onUpdate(
-            orderService.cancel(activity)
-        )
+    @EventListener(condition = "#event.activity instanceof T(com.rarible.flow.core.domain.FlowNftOrderActivityCancelList)")
+    fun orderCancelled(event: IndexerEvent): Unit = runBlocking {
+        val activity = event.activity as FlowNftOrderActivityCancelList
+        val o = orderService.cancel(activity)
+        if (event.source != Source.REINDEX) {
+            protocolEventPublisher.onUpdate(o)
+        }
     }
 
     @EventListener
     fun makeOrderInactive(event: ItemIsWithdrawn): Unit = runBlocking {
-        protocolEventPublisher.onUpdate(
-            orderService.deactivateOrdersByItem(
-                event.item,
-                LocalDateTime.ofInstant(event.activityTime, ZoneOffset.UTC)
-            )
+        val o = orderService.deactivateOrdersByItem(
+            event.item,
+            LocalDateTime.ofInstant(event.activityTime, ZoneOffset.UTC)
         )
+        if (event.source != Source.REINDEX) {
+            protocolEventPublisher.onUpdate(o)
+        }
     }
 
     @EventListener
     fun restoreOrders(event: ItemIsDeposited): Unit = runBlocking {
         val item = event.item
-        protocolEventPublisher.onUpdate(
-            orderService.restoreOrdersForItem(item)
-        )
+        val o = orderService.restoreOrdersForItem(item)
+        if (event.source != Source.REINDEX) {
+            protocolEventPublisher.onUpdate(o)
+        }
     }
 }
--- scanner/src/main/kotlin/com/rarible/flow/scanner/eventlisteners/events.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/eventlisteners/events.kt
@@ -1,15 +1,17 @@
 package com.rarible.flow.scanner.eventlisteners
 
 import com.nftco.flow.sdk.FlowAddress
+import com.rarible.blockchain.scanner.framework.data.Source
 import com.rarible.flow.core.domain.Item
 import java.time.Instant
 
 
-data class ItemIsWithdrawn(val item: Item, val from: FlowAddress, val activityTime: Instant)
+data class ItemIsWithdrawn(val item: Item, val from: FlowAddress, val activityTime: Instant, val source: Source)
 
 data class ItemIsDeposited(
     val item: Item,
     val to: FlowAddress,
     val from: FlowAddress?,
-    val activityTime: Instant
+    val activityTime: Instant,
+    val source: Source
 )
