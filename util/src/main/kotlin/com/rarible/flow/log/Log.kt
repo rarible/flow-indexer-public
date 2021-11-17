1003ab55d6c693c2a550a488c418df5ab4972ba4
zolt <timochkinea@gmail.com>
2021-11-17 10:01:36 +0700

    FB-417 - added indexes

--- util/src/main/kotlin/com/rarible/flow/log/Log.kt
+++ util/src/main/kotlin/com/rarible/flow/log/Log.kt
@@ -1,5 +1,6 @@
 package com.rarible.flow.log
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.properties.ReadOnlyProperty
@@ -14,5 +15,15 @@ class Log<in R : Any> : ReadOnlyProperty<R, Logger> {
         return javaClass.enclosingClass?.takeIf {
             it.kotlin.companionObject?.java == javaClass
         } ?: javaClass
+    }
+}
+
+@ExperimentalCoroutinesApi
+suspend fun <T> logTime(label: String, f: suspend () -> T): T {
+    val start = System.currentTimeMillis()
+    return try {
+        f()
+    } finally {
+        println("____ $label time: ${System.currentTimeMillis() - start}ms")
     }
 }
