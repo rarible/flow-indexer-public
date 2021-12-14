b939c484667dbc7f2fc3484810a532dac7d5fa90
Andrey Atapin <a.atapin@gmail.com>
2021-12-14 12:12:59 +0500

    fixed meta

--- backend-api/src/main/kotlin/com/rarible/flow/api/metaprovider/CnnNFTMetaProvider.kt
+++ backend-api/src/main/kotlin/com/rarible/flow/api/metaprovider/CnnNFTMetaProvider.kt
@@ -2,6 +2,8 @@ package com.rarible.flow.api.metaprovider
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.nftco.flow.sdk.cadence.JsonCadenceBuilder
+import com.rarible.flow.api.service.ScriptExecutor
 import com.rarible.flow.core.domain.ItemId
 import com.rarible.flow.core.domain.ItemMeta
 import com.rarible.flow.core.domain.ItemMetaAttribute
@@ -9,6 +11,9 @@ import com.rarible.flow.core.repository.ItemRepository
 import com.rarible.flow.log.Log
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.json.JacksonJsonParser
+import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBodyOrNull
@@ -16,14 +21,33 @@ import org.springframework.web.reactive.function.client.awaitBodyOrNull
 @Component
 class CnnNFTMetaProvider(
     private val itemRepository: ItemRepository,
+    private val scriptExecutor: ScriptExecutor,
+    @Value("classpath:script/cnn_meta.cdc")
+    private val scriptFile: Resource,
 ) : ItemMetaProvider {
+
+    private val scriptText = scriptFile.file.readText()
+
+    private val cadenceBuilder = JsonCadenceBuilder()
 
     private val logger by Log()
 
     override fun isSupported(itemId: ItemId): Boolean = itemId.contract.contains("CNN_NFT")
 
     override suspend fun getMeta(itemId: ItemId): ItemMeta {
-        val url = "https://onchain.mugenart.io/flow/nft/0x2cd46d41da4ce262/metadata/${itemId.tokenId}"
+        val item = itemRepository.findById(itemId).awaitSingleOrNull() ?: return emptyMeta(itemId)
+        if (item.meta.isNullOrEmpty()) return emptyMeta(itemId)
+        val meta = JacksonJsonParser().parseMap(item.meta)
+        val ipfsHash = scriptExecutor.execute(
+            code = scriptText,
+            args = mutableListOf(
+                cadenceBuilder.uint32(meta[CnnNFT::setId.name].toString()),
+                cadenceBuilder.uint32(meta[CnnNFT::editionNum.name].toString())
+            )
+        )
+
+
+        val url = "https://rarible.mypinata.cloud/ipfs/${ipfsHash}"
 
         val client = WebClient.create()
         return try {
@@ -33,32 +57,12 @@ class CnnNFTMetaProvider(
                 itemId = itemId,
                 name = data.name,
                 description = data.description,
-                attributes = data.attributes.map {
-                    ItemMetaAttribute(
-                        key = it.traitType,
-                        value = it.value
-                    )
-                } + listOf(
-                    ItemMetaAttribute(
-                        key = "backgroundColor",
-                        value = data.backgroundColor,
-                    ),
-                    ItemMetaAttribute(
-                        key = "baiduModelKey",
-                        value = data.baiduModelKey,
-                    )
-                ),
                 contentUrls = listOfNotNull(
-                    data.imageBlocto,
-                    data.icon,
                     data.image,
-                    data.imagePreview,
-                    data.imageHd,
-                    data.externalUrl,
-                    data.animationUrl,
-                    data.animationUrl2,
-                    data.youtubeUrl,
+                    data.preview,
+                    data.externalUrl
                 ),
+                attributes = data.getAttributes()
             ).apply {
                 raw = data.toString().toByteArray(charset = Charsets.UTF_8)
             }
@@ -71,38 +75,58 @@ class CnnNFTMetaProvider(
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class CnnNFTMetaBody(
-    val attributes: List<CnnNftAttr>,
-    val address: String,
-    @get:JsonProperty("type_id")
-    val typeId: Long,
-    @get:JsonProperty("token_id")
-    val tokenId: Long,
+    val name: String,
     val description: String,
+    val image: String? = null,
+    val preview: String? = null,
+
     @get:JsonProperty("external_url")
     val externalUrl: String? = null,
-    val icon: String? = null,
-    val image: String? = null,
-    @get:JsonProperty("image_preview")
-    val imagePreview: String? = null,
-    @get:JsonProperty("image_hd")
-    val imageHd: String? = null,
-    @get:JsonProperty("background_color")
-    val backgroundColor: String? = null,
-    @get:JsonProperty("animation_url")
-    val animationUrl: String? = null,
-    @get:JsonProperty("animation_url_2")
-    val animationUrl2: String? = null,
-    @get:JsonProperty("baidu_model_key")
-    val baiduModelKey: String? = null,
-    @get:JsonProperty("youtube_url")
-    val youtubeUrl: String? = null,
-    val name: String,
-    @get:JsonProperty("image_blocto")
-    val imageBlocto: String?,
-)
 
-data class CnnNftAttr(
-    @get:JsonProperty("trait_type")
-    val traitType: String,
-    val value: String?,
-)
+    @get:JsonProperty("creator_name")
+    val creatorName: String,
+
+    val seriesName: String,
+    val seriesDescription: String,
+
+    @get:JsonProperty("series_id")
+    val seriesId: Integer,
+
+    @get:JsonProperty("set_id")
+    val setId: Integer,
+
+    @get:JsonProperty("edition")
+    val edition: Integer,
+
+    @get:JsonProperty("max_editions")
+    val maxEditions: Integer
+) {
+    fun getAttributes(): List<ItemMetaAttribute> {
+        return listOf(
+            ItemMetaAttribute(
+                key = "seriesName",
+                value = seriesName
+            ),
+            ItemMetaAttribute(
+                key = "seriesDescription",
+                value = seriesDescription
+            ),
+            ItemMetaAttribute(
+                key = "seriesId",
+                value = seriesId.toString()
+            ),
+            ItemMetaAttribute(
+                key = "setId",
+                value = setId.toString()
+            ),
+            ItemMetaAttribute(
+                key = "edition",
+                value = edition.toString()
+            ),
+            ItemMetaAttribute(
+                key = "maxEditions",
+                value = maxEditions.toString()
+            )
+        )
+    }
+}
