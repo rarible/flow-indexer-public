c3ef1b1ae662a9f50597b269f0dd89e2264cb83c
zolt <timochkinea@gmail.com>
2021-12-18 12:41:50 +0700

    FB-445 fix balance processing

--- backend-core/src/main/kotlin/com/rarible/flow/core/domain/Balance.kt
+++ backend-core/src/main/kotlin/com/rarible/flow/core/domain/Balance.kt
@@ -3,7 +3,7 @@ package com.rarible.flow.core.domain
 import com.nftco.flow.sdk.FlowAddress
 import org.springframework.data.annotation.AccessType
 import org.springframework.data.annotation.Id
-import org.springframework.data.annotation.Version
+import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
 import org.springframework.data.mongodb.core.mapping.FieldType
 import java.io.Serializable
@@ -27,23 +27,20 @@ data class BalanceId(
     }
 }
 
+@Document
 data class Balance(
     val account: FlowAddress,
     val token: String,
     @Field(targetType = FieldType.DECIMAL128)
     val balance: BigDecimal = BigDecimal.ZERO,
-
     @Field(targetType = FieldType.DATE_TIME)
     val lastUpdatedAt: Instant = Instant.now(),
-    @Version
-    val version: Long? = null
 ) {
-
     @get:Id
     @get:AccessType(AccessType.Type.PROPERTY)
     var id: BalanceId
         get() = BalanceId(account, token)
-        set(value) {}
+        set(_) {}
 
     fun withdraw(amount: BigDecimal): Balance {
         return this.copy(balance = this.balance.minus(amount))
