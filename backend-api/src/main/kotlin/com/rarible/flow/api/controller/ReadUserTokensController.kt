fcd4d5404487434c1565b846d71884fd4a738444
Andrey Atapin <1181403+atapin@users.noreply.github.com>
2021-12-20 18:13:07 +0500

    FB-482 better logging for coroutine (#296)

--- backend-api/src/main/kotlin/com/rarible/flow/api/controller/ReadUserTokensController.kt
+++ backend-api/src/main/kotlin/com/rarible/flow/api/controller/ReadUserTokensController.kt
@@ -1,6 +1,7 @@
 package com.rarible.flow.api.controller
 
 import com.rarible.flow.api.service.UserStorageService
+import com.rarible.flow.log.Log
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -17,7 +18,19 @@ class ReadUserTokensController(private val userStorageService: UserStorageServic
     @GetMapping("/v0.1/newLogin/{address}")
     suspend fun newLogin(@PathVariable address: String): String {
         val flowAddress = address.flowAddress()!!
-        GlobalScope.launch { userStorageService.scanNFT(flowAddress) }
+        GlobalScope
+            .launch { userStorageService.scanNFT(flowAddress) }
+            .invokeOnCompletion { completionCause ->
+                if(completionCause == null) {
+                    logger.info("New login for {} completed", address)
+                } else {
+                    logger.error("New login error", completionCause)
+                }
+            }
         return "OK"
+    }
+
+    companion object {
+        val logger by Log()
     }
 }
