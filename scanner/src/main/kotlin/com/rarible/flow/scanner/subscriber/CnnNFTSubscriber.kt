e6a4d3e88784dac561d8549a280f526458547cad
Andrey Atapin <a.atapin@gmail.com>
2021-12-21 17:41:22 +0500

    Release 1.0 merge

--- scanner/src/main/kotlin/com/rarible/flow/scanner/subscriber/CnnNFTSubscriber.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/subscriber/CnnNFTSubscriber.kt
@@ -1,104 +1,47 @@
 package com.rarible.flow.scanner.subscriber
 
 import com.nftco.flow.sdk.FlowChainId
-import com.nftco.flow.sdk.cadence.*
-import com.rarible.blockchain.scanner.flow.client.FlowBlockchainBlock
 import com.rarible.blockchain.scanner.flow.client.FlowBlockchainLog
 import com.rarible.blockchain.scanner.flow.model.FlowDescriptor
-import com.rarible.flow.core.domain.BaseActivity
-import com.rarible.flow.core.domain.BurnActivity
-import com.rarible.flow.core.domain.DepositActivity
-import com.rarible.flow.core.domain.MintActivity
-import com.rarible.flow.core.domain.WithdrawnActivity
-import com.rarible.flow.events.EventMessage
-import com.rarible.flow.scanner.model.parse
+import com.rarible.flow.core.domain.FlowLogType
+import com.rarible.flow.events.EventId
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.springframework.stereotype.Component
-import java.time.Instant
 
 @ExperimentalCoroutinesApi
 @Component
-class CnnNFTSubscriber : BaseItemHistoryFlowLogSubscriber() {
+class CnnNFTSubscriber : BaseFlowLogEventSubscriber() {
+    val events = setOf("Minted", "Withdraw", "Deposit", "NFTDestroyed")
+
     override val descriptors: Map<FlowChainId, FlowDescriptor>
         get() = mapOf(
-            FlowChainId.MAINNET to FlowDescriptor(
-                id = "CnnNFTSubscriber",
-                events = setOf(
-                    "A.329feb3ab062d289.CNN_NFT.Minted",
-                    "A.329feb3ab062d289.CNN_NFT.Withdraw",
-                    "A.329feb3ab062d289.CNN_NFT.Deposit",
-                    "A.329feb3ab062d289.CNN_NFT.NFTDestroyed",
-                ),
-                collection = collection,
+            FlowChainId.MAINNET to flowDescriptor(
+                contract = "CNN_NFT",
+                address = "329feb3ab062d289",
+                events = events,
+                dbCollection = collection,
                 startFrom = 15640000L,
             ),
-            FlowChainId.TESTNET to FlowDescriptor(
-                id = "CnnNFTSubscriber",
-                events = setOf(
-                    "A.ebf4ae01d1284af8.CNN_NFT.Minted",
-                    "A.ebf4ae01d1284af8.CNN_NFT.Withdraw",
-                    "A.ebf4ae01d1284af8.CNN_NFT.Deposit",
-                    "A.ebf4ae01d1284af8.CNN_NFT.NFTDestroyed",
-                ),
-                collection = collection,
+            FlowChainId.TESTNET to flowDescriptor(
+                contract = "CNN_NFT",
+                address = "ebf4ae01d1284af8",
+                events = events,
+                dbCollection = collection,
                 startFrom = 53489946L
             ),
-            FlowChainId.EMULATOR to FlowDescriptor(
-                id = "CnnNFTSubscriber",
-                events = setOf(
-                    "A.f8d6e0586b0a20c7.CNN_NFT.Mint",
-                    "A.f8d6e0586b0a20c7.CNN_NFT.Withdraw",
-                    "A.f8d6e0586b0a20c7.CNN_NFT.Deposit",
-                    "A.f8d6e0586b0a20c7.CNN_NFT.NFTDestroyed",
-                ),
-                collection = collection,
+            FlowChainId.EMULATOR to flowDescriptor(
+                contract = "CNN_NFT",
+                address = "f8d6e0586b0a20c7",
+                events = events,
+                dbCollection = collection,
             ),
         )
 
-    override suspend fun activity(block: FlowBlockchainBlock, log: FlowBlockchainLog, msg: EventMessage): BaseActivity {
-        val id: NumberField by msg.fields
-        val tokenId = id.toLong()!!
-        val contract = msg.eventId.collection()
-        val timestamp = Instant.ofEpochMilli(block.timestamp)
-        val eventId = "${msg.eventId}"
-        return when {
-            eventId.endsWith("Minted") -> {
-                MintActivity(
-                    owner = msg.eventId.contractAddress.formatted,
-                    contract = contract,
-                    tokenId = tokenId,
-                    timestamp = timestamp,
-                    value = 1L, royalties = emptyList(),
-                    metadata = emptyMap()
-                )
-            }
-
-            eventId.endsWith("Withdraw") -> {
-                val from: OptionalField by msg.fields
-                WithdrawnActivity(
-                    contract = contract,
-                    tokenId = tokenId,
-                    from = (from.value as? AddressField)?.value,
-                    timestamp = timestamp
-                )
-            }
-            eventId.endsWith("Deposit") -> {
-                val to: OptionalField by msg.fields
-                DepositActivity(
-                    contract = contract,
-                    tokenId = tokenId,
-                    to = (to.value as? AddressField)?.value,
-                    timestamp = timestamp
-                )
-            }
-            eventId.endsWith("NFTDestroyed") -> {
-                BurnActivity(
-                    contract = contract,
-                    tokenId = tokenId,
-                    timestamp = timestamp
-                )
-            }
-            else -> throw IllegalStateException("Unsupported eventId: $eventId")
-        }
+    override suspend fun eventType(log: FlowBlockchainLog): FlowLogType = when(EventId.of(log.event.type).eventName) {
+        "Withdraw" -> FlowLogType.WITHDRAW
+        "Deposit" -> FlowLogType.DEPOSIT
+        "Mint" -> FlowLogType.MINT
+        "NFTDestroyed" -> FlowLogType.BURN
+        else ->  throw IllegalStateException("Unsupported event type: ${log.event.type}")
     }
 }
