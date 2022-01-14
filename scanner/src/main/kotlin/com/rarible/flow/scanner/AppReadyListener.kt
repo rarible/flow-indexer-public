1212f48c5f1dd024e573f485c4cf67d6368de2cd
zolt <timochkinea@gmail.com>
2022-01-14 17:57:03 +0700

    FB-516 DisruptArt logs and activities

--- scanner/src/main/kotlin/com/rarible/flow/scanner/AppReadyListener.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/AppReadyListener.kt
@@ -27,16 +27,18 @@ class AppReadyListener(
             ItemCollection(id = "A.01658d9b94068f3c.MotoGPCard", name = "MotoGP™ Ignition", owner = FlowAddress("0x01658d9b94068f3c"), symbol = "MotoGP™", features = setOf("BURN")),
             ItemCollection(id = "A.01658d9b94068f3c.TopShot", name = "NBA Top Shot", owner = FlowAddress("0x01658d9b94068f3c"), symbol = "NBA TS", features = setOf("BURN")),
             ItemCollection(id = "A.01658d9b94068f3c.Evolution", name = "Evolution", owner = FlowAddress("0x01658d9b94068f3c"), symbol = "EVOLUTION", features = setOf("BURN")),
-            ItemCollection(id = "A.ebf4ae01d1284af8.MugenNFT", name = "Mugen", owner = FlowAddress("0xebf4ae01d1284af8"), symbol = "MUGEN", features = setOf()),
+            ItemCollection(id = "A.ebf4ae01d1284af8.MugenNFT", name = "Mugen", owner = FlowAddress("0xebf4ae01d1284af8"), symbol = "MUGEN", features = emptySet()),
             ItemCollection(id = "A.ebf4ae01d1284af8.CNN_NFT", name = "CNN", owner = FlowAddress("0xebf4ae01d1284af8"), symbol = "CNN", features = setOf("BURN")),
+            ItemCollection(id = "A.439c2b49c0b2f62b.DisruptArt", name = "DisruptArt", owner = FlowAddress("0x439c2b49c0b2f62b"), symbol = "DA", features = emptySet()),
         ),
         FlowChainId.MAINNET to listOf(
             ItemCollection(id = "A.01ab36aaf654a13e.RaribleNFT", name = "Rarible", owner = FlowAddress("0x01ab36aaf654a13e"), symbol = "RARIBLE", features = setOf("SECONDARY_SALE_FEES", "BURN")),
             ItemCollection(id = "A.a49cc0ee46c54bfb.MotoGPCard", name = "MotoGP™ Ignition", owner = FlowAddress("0xa49cc0ee46c54bfb"), symbol = "MotoGP™", features = setOf("BURN")),
             ItemCollection(id = "A.0b2a3299cc857e29.TopShot", name = "NBA Top Shot", owner = FlowAddress("0x0b2a3299cc857e29"), symbol = "NBA TS", features = setOf("BURN")),
             ItemCollection(id = "A.f4264ac8f3256818.Evolution", name = "Evolution", owner = FlowAddress("0xf4264ac8f3256818"), symbol = "EVOLUTION", features = setOf("BURN")),
-            ItemCollection(id = "A.2cd46d41da4ce262.MugenNFT", name = "Mugen", owner = FlowAddress("0x2cd46d41da4ce262"), symbol = "MUGEN", features = setOf()),
+            ItemCollection(id = "A.2cd46d41da4ce262.MugenNFT", name = "Mugen", owner = FlowAddress("0x2cd46d41da4ce262"), symbol = "MUGEN", features = emptySet()),
             ItemCollection(id = "A.329feb3ab062d289.CNN_NFT", name = "CNN", owner = FlowAddress("0x329feb3ab062d289"), symbol = "CNN", features = setOf("BURN")),
+            ItemCollection(id = "A.cd946ef9b13804c6.DisruptArt", name = "DisruptArt", owner = FlowAddress("0xcd946ef9b13804c6"), symbol = "DISRUPT ART", features = emptySet()),
         )
     )
 
