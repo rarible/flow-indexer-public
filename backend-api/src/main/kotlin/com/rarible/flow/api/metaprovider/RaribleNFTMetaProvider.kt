3a929c23617558ac26282ce17a1c242ce5364121
Andrey Atapin <a.atapin@gmail.com>
2022-01-18 15:12:30 +0500

    FB-527 fixed RaribleNFT meta loading

--- backend-api/src/main/kotlin/com/rarible/flow/api/metaprovider/RaribleNFTMetaProvider.kt
+++ backend-api/src/main/kotlin/com/rarible/flow/api/metaprovider/RaribleNFTMetaProvider.kt
@@ -2,19 +2,23 @@ package com.rarible.flow.api.metaprovider
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.rarible.flow.core.domain.Item
 import com.rarible.flow.core.domain.ItemId
 import com.rarible.flow.core.domain.ItemMeta
 import com.rarible.flow.core.domain.ItemMetaAttribute
 import com.rarible.flow.core.repository.ItemRepository
+import com.rarible.flow.core.repository.coFindById
 import com.rarible.flow.log.Log
-import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.springframework.boot.json.JacksonJsonParser
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBodyOrNull
+import reactor.kotlin.extra.retry.retryExponentialBackoff
+import java.time.Duration
 
 @Component
 class RaribleNFTMetaProvider(
+    private val pinataClient: WebClient,
     private val itemRepository: ItemRepository
 ) : ItemMetaProvider {
 
@@ -23,45 +27,41 @@ class RaribleNFTMetaProvider(
     override fun isSupported(itemId: ItemId): Boolean = itemId.contract.contains("RaribleNFT")
 
     override suspend fun getMeta(itemId: ItemId): ItemMeta {
-        val item = itemRepository.findById(itemId).awaitSingleOrNull()
-            ?: return emptyMeta(itemId)
-        var url = item.meta ?: return emptyMeta(itemId)
+        return try {
+            itemRepository.coFindById(itemId)
+                ?.let { item -> readUrl(item) }
+                ?.let { ipfs ->
+                    pinataClient
+                        .get()
+                        .uri("/$ipfs")
+                        .retrieve()
+                        .bodyToMono(RaribleNFTMetaBody::class.java)
+                        .retryExponentialBackoff(3, Duration.ofMillis(500))
+                        .awaitFirstOrNull()
+                }
+                ?.toItemMeta(itemId)
+        } catch (e: Exception) {
+            logger.warn("Failed RaribleNFTMetaProvider.getMeta({})", itemId, e)
+            null
+        } ?: ItemMeta.empty(itemId)
+    }
+
+    fun readUrl(item: Item): String? {
+        var url = item.meta ?: return null
         if (url.startsWith("{")) {
             url = JacksonJsonParser().parseMap(url)["metaURI"] as String?
-                ?: return emptyMeta(itemId)
+                ?: return null
         }
-
 
         if (url.startsWith("ipfs://")) {
             url = url.substring("ipfs:/".length)
         }
 
         if (url.isEmpty()) {
-            return emptyMeta(itemId)
+            return null
         }
 
-        val client = WebClient.create("https://rarible.mypinata.cloud/")
-        return try {
-            val data = client.get().uri(url)
-                .retrieve().awaitBodyOrNull<RaribleNFTMetaBody>() ?: return emptyMeta(itemId)
-            ItemMeta(
-                itemId = itemId,
-                name = data.name,
-                description = data.description,
-                attributes = data.attributes.map {
-                    ItemMetaAttribute(
-                        key = it.key ?: it.traitType!!,
-                        value = it.value
-                    )
-                },
-                contentUrls = listOfNotNull(data.image, data.animationUrl),
-            ).apply {
-                raw = data.toString().toByteArray(charset = Charsets.UTF_8)
-            }
-        } catch (e: Exception) {
-            logger.warn(e.message, e)
-            return emptyMeta(itemId)
-        }
+        return url
     }
 }
 
@@ -73,7 +73,24 @@ data class RaribleNFTMetaBody(
     @get:JsonProperty("animation_url")
     val animationUrl: String? = null,
     val attributes: List<RaribleNftAttr>
-)
+) {
+    fun toItemMeta(itemId: ItemId): ItemMeta {
+        return ItemMeta(
+            itemId = itemId,
+            name = name,
+            description = description,
+            attributes = attributes.map {
+                ItemMetaAttribute(
+                    key = it.key ?: it.traitType!!,
+                    value = it.value
+                )
+            },
+            contentUrls = listOfNotNull(image, animationUrl),
+        ).apply {
+            raw = toString().toByteArray(charset = Charsets.UTF_8)
+        }
+    }
+}
 
 data class RaribleNftAttr(
     val key: String?,
---
+++ backend-api/src/test/kotlin/com/rarible/flow/api/mocks.kt
@@ -0,0 +1,60 @@
+package com.rarible.flow.api
+
+import com.nftco.flow.sdk.FlowChainId
+import com.nftco.flow.sdk.FlowScript
+import com.nftco.flow.sdk.FlowScriptResponse
+import com.rarible.flow.api.service.ScriptExecutor
+import com.rarible.flow.core.config.AppProperties
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpStatus
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import java.io.ByteArrayInputStream
+import java.util.concurrent.CompletableFuture
+
+object mocks {
+    fun scriptExecutor(results: Map<String, String>): ScriptExecutor {
+        return ScriptExecutor(
+            mockk() {
+                results.forEach { (script, response) ->
+                    every {
+                        executeScriptAtLatestBlock(eq(FlowScript(script)), any())
+                    } returns CompletableFuture.completedFuture(
+                        FlowScriptResponse(response.toByteArray())
+                    )
+                }
+            },
+            AppProperties("test", "", FlowChainId.EMULATOR)
+        )
+    }
+
+    fun scriptExecutor(vararg results: Pair<String, String>): ScriptExecutor {
+        return scriptExecutor(mapOf(*results))
+    }
+
+    fun resource(script: String, fileName: String? = null) = mockk<Resource>() {
+        every {
+            inputStream
+        } returns ByteArrayInputStream(script.toByteArray())
+
+        every {
+            filename
+        } returns (fileName ?: script)
+    }
+
+    fun webClient(expectedPath: String, response: String) = WebClient.builder()
+        .exchangeFunction { req ->
+            req.url().path shouldBe expectedPath
+
+            Mono.just(
+                ClientResponse.create(HttpStatus.OK)
+                    .header("content-type", "application/json")
+                    .body(response)
+                    .build()
+            )
+        }.build()
+}
