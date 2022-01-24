508e50933316d3c48d07c04a58961791fc45f7a7
zolt <timochkinea@gmail.com>
2022-01-24 14:23:49 +0700

    FB-542 shutdown disruptart subscriber

--- scanner/src/main/kotlin/com/rarible/flow/scanner/subscriber/DisruptArtSubscriber.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/subscriber/DisruptArtSubscriber.kt
@@ -5,9 +5,11 @@ import com.rarible.blockchain.scanner.flow.client.FlowBlockchainLog
 import com.rarible.blockchain.scanner.flow.model.FlowDescriptor
 import com.rarible.flow.core.domain.FlowLogType
 import com.rarible.flow.events.EventId
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.stereotype.Component
 
 @Component
+@ConditionalOnExpression("false")
 class DisruptArtSubscriber: BaseFlowLogEventSubscriber() {
 
     private val contractName = "DisruptArt"
