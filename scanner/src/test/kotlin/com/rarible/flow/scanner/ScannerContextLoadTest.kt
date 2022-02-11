d2c4719c78cd49a441acda5704ba0db39273fb76
Andrey Atapin <a.atapin@gmail.com>
2022-02-11 11:12:21 +0500

    FB-562 test fixes

--- scanner/src/test/kotlin/com/rarible/flow/scanner/ScannerContextLoadTest.kt
+++ scanner/src/test/kotlin/com/rarible/flow/scanner/ScannerContextLoadTest.kt
@@ -1,6 +1,5 @@
 package com.rarible.flow.scanner
 
-import com.rarible.flow.log.Log
 import org.junit.jupiter.api.Test
 
 
@@ -10,7 +9,4 @@ class ScannerContextLoadTest: BaseIntegrationTest() {
     @Test
     fun contextLoads() {}
 
-    companion object {
-        val logger by Log()
-    }
 }
