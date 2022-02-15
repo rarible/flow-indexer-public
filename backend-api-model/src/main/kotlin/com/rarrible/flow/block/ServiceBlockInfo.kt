6a16eb0d9c55422411e61960cb7bdebc98144d98
zolt <timochkinea@gmail.com>
2022-02-15 17:52:39 +0700

    FB-580 increased scanner version, improved block scanner info api answer

--- backend-api-model/src/main/kotlin/com/rarrible/flow/block/ServiceBlockInfo.kt
+++ backend-api-model/src/main/kotlin/com/rarrible/flow/block/ServiceBlockInfo.kt
@@ -1,8 +1,26 @@
 package com.rarrible.flow.block
 
+import java.util.concurrent.TimeUnit
+
 data class ServiceBlockInfo(
     val lastBlockInIndexer: BlockInfo,
     val lastBlockInBlockchain: BlockInfo
-)
+) {
+
+    val blockLatency: Long = lastBlockInBlockchain.blockHeight - lastBlockInIndexer.blockHeight
+    val timeLatencyMs: Long = lastBlockInBlockchain.timestamp - lastBlockInIndexer.timestamp
+    val timeLatency: String = timeLatencyMs.toHoursStr()
+}
 
 data class BlockInfo(val blockHeight: Long, val timestamp: Long)
+
+private fun Long.toHoursStr(): String {
+    val hours = TimeUnit.MILLISECONDS.toHours(this)
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(this) - TimeUnit.HOURS.toMinutes(
+        hours
+    )
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(this) - TimeUnit.MINUTES.toSeconds(
+        TimeUnit.MILLISECONDS.toMinutes(this)
+    )
+    return "${"%02d".format(hours)}h ${"%02d".format(minutes)}m ${"%02d".format(seconds)}s"
+}
