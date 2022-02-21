b4184bb4622d311bfb06cfb04b477f5e63153cf3
Andrey Atapin <a.atapin@gmail.com>
2022-02-22 01:34:26 +0500

    meta fix

--- backend-api/src/main/kotlin/com/rarible/flow/api/metaprovider/KicksMetaProvider.kt
+++ backend-api/src/main/kotlin/com/rarible/flow/api/metaprovider/KicksMetaProvider.kt
@@ -36,7 +36,7 @@ class KicksMetaScript(
             { response ->
                 response as OptionalField
                 response.value?.let {
-                    Flow.unmarshall(OneFootballMeta::class, it)
+                    Flow.unmarshall(KicksMeta::class, it)
                 }
             }
         )
@@ -46,10 +46,10 @@ class KicksMetaScript(
 @Component
 class KicksMetaProvider(
     val itemRepository: ItemRepository,
-    val metaScript: OneFootballMetaScript
+    val metaScript: KicksMetaScript
 ): ItemMetaProvider {
 
-    override fun isSupported(itemId: ItemId): Boolean = Contracts.ONE_FOOTBALL.supports(itemId)
+    override fun isSupported(itemId: ItemId): Boolean = Contracts.KICKS.supports(itemId)
 
     override suspend fun getMeta(itemId: ItemId): ItemMeta {
         return itemRepository
@@ -64,15 +64,19 @@ class KicksMetaProvider(
 data class KicksMeta(
     val title: String,
     val description: String,
-    val media: List<String>,
+    val video: String?,
+    val image: String?,
     val redeemed: Boolean,
+    val size: String?,
     val taggedTopShot: String?
 ): MetaBody {
     override fun toItemMeta(itemId: ItemId): ItemMeta {
         val attributes = listOfNotNull(
             ItemMetaAttribute("redeemed", redeemed.toString()),
-            taggedTopShot?.let { ItemMetaAttribute("taggedTopShot", it) }
+            taggedTopShot?.let { ItemMetaAttribute("taggedTopShot", it) },
+            size?.let { ItemMetaAttribute("size", size) }
         )
+        val media = listOfNotNull(video, image)
         return ItemMeta(itemId, title, description, attributes, media)
     }
 }
@@ -80,16 +84,23 @@ data class KicksMeta(
 class KicksMetaConverter: JsonCadenceConverter<KicksMeta> {
     override fun unmarshall(value: Field<*>, namespace: CadenceNamespace): KicksMeta {
         return com.nftco.flow.sdk.cadence.unmarshall(value) {
+            val meta: Map<String, String> = dictionaryMap("metadata") { k, v ->
+                val key = string(k)
+                if(listOf("video", "image", "size").contains(key)) {
+                    key to string(v)
+                } else if(key == "redeemed") {
+                    key to boolean(v).toString()
+                } else key to ""
+            }.filterValues { it != "" }
+
             KicksMeta(
                 title = string(compositeValue.getRequiredField("title")),
                 description = string(compositeValue.getRequiredField("description")),
-                redeemed = boolean(compositeValue.getRequiredField("redeemed")),
-                media = arrayValues("media") { singleMedia ->
-                     string(singleMedia)
-                },
-                taggedTopShot = optional("taggedTopShot") {
-                    string(it)
-                }
+                redeemed = meta["redeemed"]?.toBoolean() ?: false,
+                size = meta["size"],
+                taggedTopShot = meta["taggedTopShot"],
+                video = meta["video"],
+                image = meta["image"]
             )
         }
     }
--- backend-api/src/main/resources/script/meta/kicks.cdc
+++ backend-api/src/main/resources/script/meta/kicks.cdc
@@ -4,24 +4,15 @@ import Kicks from 0xf3cc54f4d91c2f6c
 pub struct Meta {
     pub let title: String
     pub let description: String
-    pub let media: [String]
-    pub let redeemed: Bool
-    pub let taggedTopShot: String?
     pub let metadata: {String: AnyStruct}
 
     init(
         title: String,
         description: String,
-        media: [String],
-        redeemed: Bool,
-        taggedTopShot: String?,
         metadata: {String: AnyStruct}
     ) {
         self.title = title
         self.description = description
-        self.media = media
-        self.redeemed = redeemed
-        self.taggedTopShot = taggedTopShot
         self.metadata = metadata
     }
 }
@@ -31,47 +22,16 @@ pub fun main(owner: Address, id: UInt64): Meta? {
        .getCapability(Kicks.CollectionPublicPath)
        .borrow<&{Kicks.KicksCollectionPublic}>()
 
-     if col == nil { return nil }
+    if col == nil { return nil }
 
-     let nft = col!.borrowSneaker(id: id)
-     if nft == nil { return nil }
+    let nft = col!.borrowSneaker(id: id)
+    if nft == nil { return nil }
 
     let metadata = nft!.getMetadata()
-    var images: [String] = []
-    var videos: [String] = []
-    var media: [String] = []
-
-    if let mediaValue = metadata["media"] {
-        if let supportedMedia = mediaValue as? {String: [String]} {
-
-            if let images = supportedMedia["image"] {
-                images.appendAll(images)
-            }
-            if let videos = supportedMedia["video"] {
-                videos.appendAll(videos)
-            }
-        } else {
-            panic("nothing1")
-        }
-    } else {
-        panic("nothing")
-    }
-
-    
-
-    media.appendAll(videos)
-    media.appendAll(images)
-
-
-    let taggedTopShot = metadata["taggedTopShot"] as? String
-    let redeemed = metadata["redeemed"] as? Bool
 
     return Meta(
         title: nft!.name(),
         description: nft!.description(),
-        media: media,
-        redeemed: redeemed ?? false,
-        taggedTopShot: taggedTopShot,
         metadata: metadata
     )
 }
