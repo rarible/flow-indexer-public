a020ab2bc2038f321522e2b7684728b63843ac82
Andrey Atapin <a.atapin@gmail.com>
2022-03-09 12:40:52 +0500

    FB-647 task update controller

--- backend-api/src/test/kotlin/com/rarible/flow/api/controller/TaskControllerTest.kt
+++ backend-api/src/test/kotlin/com/rarible/flow/api/controller/TaskControllerTest.kt
@@ -1,22 +1,10 @@
 package com.rarible.flow.api.controller
 
 import com.mongodb.client.result.UpdateResult
-import com.nftco.flow.sdk.FlowAddress
 import com.ninjasquad.springmockk.MockkBean
 import com.rarible.flow.api.TestPropertiesConfiguration
-import com.rarible.flow.api.http.shouldGetBadRequest
-import com.rarible.flow.api.http.shouldGetPaginatedResult
-import com.rarible.flow.core.domain.*
-import com.rarible.flow.core.repository.OrderFilter
-import com.rarible.flow.randomFlowAddress
-import com.rarible.flow.randomLong
-import com.rarible.protocol.dto.FlowOrdersPaginationDto
-import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
-import io.mockk.coVerify
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flowOf
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
@@ -27,9 +15,6 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 import reactor.kotlin.core.publisher.toMono
-import java.math.BigDecimal
-import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 @WebFluxTest(
     controllers = [TaskController::class],
