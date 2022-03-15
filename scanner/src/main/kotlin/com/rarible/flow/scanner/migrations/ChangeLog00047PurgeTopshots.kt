535dfda35d22876ec9e495a5f5b1a66add9dc471
Andrey Atapin <a.atapin@gmail.com>
2022-03-15 14:07:02 +0500

    FB-669 drop subscriber

--- scanner/src/main/kotlin/com/rarible/flow/scanner/migrations/ChangeLog00047PurgeTopshots.kt
+++ scanner/src/main/kotlin/com/rarible/flow/scanner/migrations/ChangeLog00047PurgeTopshots.kt
@@ -1,11 +1,17 @@
 package com.rarible.flow.scanner.migrations
 
+import com.rarible.core.task.Task
 import com.rarible.flow.Contracts
 import com.rarible.flow.core.config.AppProperties
 import com.rarible.flow.scanner.service.CollectionService
+import com.rarible.flow.scanner.subscriber.flowDescriptorName
 import io.mongock.api.annotations.ChangeUnit
 import io.mongock.api.annotations.Execution
 import io.mongock.api.annotations.RollbackExecution
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.isEqualTo
+import org.springframework.data.mongodb.core.remove
 
 @ChangeUnit(
     id = "ChangeLog00047PurgeTopshots",
@@ -14,7 +20,8 @@ import io.mongock.api.annotations.RollbackExecution
 )
 class ChangeLog00047PurgeTopshots(
     private val collectionService: CollectionService,
-    private val appProperties: AppProperties
+    private val appProperties: AppProperties,
+    private val mongoTemplate: MongoTemplate
 ) {
 
     @Execution
@@ -22,6 +29,10 @@ class ChangeLog00047PurgeTopshots(
         collectionService
             .purgeCollectionAsync(Contracts.TOPSHOT, appProperties.chainId)
             .blockLast()
+
+        mongoTemplate.remove<Task>(
+            Query(Task::param isEqualTo Contracts.TOPSHOT.flowDescriptorName())
+        )
     }
 
     @RollbackExecution
