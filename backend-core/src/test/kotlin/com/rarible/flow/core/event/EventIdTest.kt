3cb7d05d4b33c3418bb8e49f845a246b1bbea866
sanufriev <s.anufriev@rarible.com>
2023-02-11 23:31:05 +0700

    feat: Remove gradle

--- backend-core/src/test/kotlin/com/rarible/flow/core/event/EventIdTest.kt
+++ backend-core/src/test/kotlin/com/rarible/flow/core/event/EventIdTest.kt
@@ -1,17 +1,16 @@
-package com.rarible.flow.events
+package com.rarible.flow.core.event
 
-
+import com.nftco.flow.sdk.FlowAddress
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.comparables.shouldBeEqualComparingTo
 import io.kotest.matchers.equality.shouldBeEqualToComparingFields
-import org.junit.jupiter.api.Test
-import org.onflow.sdk.Address
+import org.junit.jupiter.api.Assertions.*
 
 class EventIdTest: FunSpec({
 
     test("EventId de-/serialization") {
         val str = "A.0b2a3299cc857e29.TopShot.MomentMinted"
-        val id = EventId("A", Address("0b2a3299cc857e29"), "TopShot", "MomentMinted")
+        val id = EventId("A", FlowAddress("0b2a3299cc857e29"), "TopShot", "MomentMinted")
 
         EventId.of(str) shouldBeEqualToComparingFields id
         id.toString() shouldBeEqualComparingTo str
