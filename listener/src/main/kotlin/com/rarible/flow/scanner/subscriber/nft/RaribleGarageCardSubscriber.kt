27d72bb5bd4eb600122013ef37ee50cdea0eafd7
sanufriev <s.anufriev@rarible.com>
2023-02-24 22:24:15 +0700

    feat: Remove unneeded annotation

--- listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/nft/RaribleGarageCardSubscriber.kt
+++ listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/nft/RaribleGarageCardSubscriber.kt
@@ -2,11 +2,9 @@ package com.rarible.flow.scanner.subscriber.nft
 
 import com.rarible.flow.Contracts
 import com.rarible.flow.scanner.subscriber.NonFungibleTokenSubscriber
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.stereotype.Component
 
 @Component
-@ConditionalOnExpression("false")
 class RaribleGarageCardSubscriber: NonFungibleTokenSubscriber() {
     override val name = "rarible_card"
     override val contract = Contracts.RARIBLE_GARAGE_CARD
