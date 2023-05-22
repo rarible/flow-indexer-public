f633162d28b3a9e6d0422ebfbc3518e80b7011ad
sanufriev <s.anufriev@rarible.com>
2023-05-22 23:45:08 +0700

    feat: Fix build

--- listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/meta/HWOtherGarageCardV2MetaSubscriber.kt
+++ listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/meta/HWOtherGarageCardV2MetaSubscriber.kt
@@ -4,7 +4,7 @@ import com.rarible.flow.Contracts
 import org.springframework.stereotype.Component
 
 //@Component
-class HWOtherGarageCardV2MetaSubscriber: HWGarageCardV2MetaSubscriber() {
-    override val name = "hw_other_meta_card_v2"
-    override val contract = Contracts.HW_OTHER_GARAGE_PM_V2
-}
+//class HWOtherGarageCardV2MetaSubscriber: HWGarageCardV2MetaSubscriber() {
+//    override val name = "hw_other_meta_card_v2"
+//    override val contract = Contracts.HW_OTHER_GARAGE_PM_V2
+//}
--- listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/meta/RaribleGarageCardV2MetaSubscriber.kt
+++ listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/meta/RaribleGarageCardV2MetaSubscriber.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 
 //@Component
 //@EnableRaribleCardV2Meta
-class RaribleGarageCardV2MetaSubscriber: HWGarageCardV2MetaSubscriber() {
-    override val name = "rarible_meta_card_v2"
-    override val contract = Contracts.RARIBLE_GARAGE_PM_V2
-}
+//class RaribleGarageCardV2MetaSubscriber: HWGarageCardV2MetaSubscriber() {
+//    override val name = "rarible_meta_card_v2"
+//    override val contract = Contracts.RARIBLE_GARAGE_PM_V2
+//}
