344a945d09f595d236845ea6248c2fa086b4c384
sanufriev <s.anufriev@rarible.com>
2023-06-26 19:28:45 +0700

    PT-3176 Improve data parsing

--- core/src/test/kotlin/com/rarible/flow/core/domain/ItemIdTest.kt
+++ core/src/test/kotlin/com/rarible/flow/core/domain/ItemIdTest.kt
@@ -2,27 +2,54 @@ package com.rarible.flow.core.domain
 
 import com.nftco.flow.sdk.FlowChainId
 import com.rarible.flow.Contracts
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-internal class ItemIdTest: FunSpec({
-    test("parse id with default delimiter") {
-        ItemId.parse("${Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET)}:1") shouldBe ItemId(
-            Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET), 1L
+internal class ItemIdTest {
+
+    @Test
+    fun `parse id with default delimiter`() {
+        assertThat(
+            ItemId.parse("${Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET)}:1")
+        ).isEqualTo(
+            ItemId(Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET), 1L)
         )
     }
 
-    test("parse id with custom delimiter") {
-        ItemId.parse("${Contracts.SOFT_COLLECTION.fqn(FlowChainId.TESTNET)}.1", '.') shouldBe ItemId(
-            Contracts.SOFT_COLLECTION.fqn(FlowChainId.TESTNET), 1L
+    @Test
+    fun `parse id with custom delimiter`() {
+        assertThat(
+            ItemId.parse("${Contracts.SOFT_COLLECTION.fqn(FlowChainId.TESTNET)}.1", '.')
+        ).isEqualTo(
+            ItemId(Contracts.SOFT_COLLECTION.fqn(FlowChainId.TESTNET), 1L)
         )
     }
 
-    test("toString() with default delimiter") {
-        ItemId(Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET), 1L).toString() shouldBe "${Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET)}:1"
+    @Test
+    fun `toString() with default delimiter`() {
+        assertThat(
+            ItemId(Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET), 1L).toString()
+        ).isEqualTo(
+            "${Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET)}:1"
+        )
     }
 
-    test("toString() with custom delimiter") {
-        ItemId(Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET), 1L, '-').toString() shouldBe "${Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET)}-1"
+    @Test
+    fun `toString() with custom delimiter`() {
+        assertThat(
+            ItemId(Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET), 1L, '-').toString()
+        ).isEqualTo(
+            "${Contracts.RARIBLE_NFTV2.fqn(FlowChainId.TESTNET)}-1"
+        )
     }
-})
+
+    @Test
+    fun `ignore hex prefix`() {
+        listOf(
+            "A.80102bce1de42dc4.HWGarageCard:155",
+            "A.0x80102bce1de42dc4.HWGarageCard:155",
+        ).onEach {
+            assertThat(ItemId.parse(it).toString()).isEqualTo("A.80102bce1de42dc4.HWGarageCard:155")
+        }
+    }
+}
