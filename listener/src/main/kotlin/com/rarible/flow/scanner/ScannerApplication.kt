90ccc519af9f248b7bb545104108ecd9efb7889a
Dmitry Saraykin <d.saraykin@rarible.com>
2023-09-13 09:44:20 +0300

    feat(PT-3630): kafka shutdown hook

--- listener/src/main/kotlin/com/rarible/flow/scanner/ScannerApplication.kt
+++ listener/src/main/kotlin/com/rarible/flow/scanner/ScannerApplication.kt
@@ -1,9 +1,10 @@
 package com.rarible.flow.scanner
 
+import com.rarible.core.kafka.KafkaShutdownHook
 import com.rarible.core.kafka.RaribleKafkaConsumerWorker
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class ScannerApplication(
@@ -15,5 +16,8 @@ class ScannerApplication(
 }
 
 fun main(args: Array<String>) {
-    runApplication<ScannerApplication>(*args)
+    val app = SpringApplication(ScannerApplication::class.java)
+    app.setRegisterShutdownHook(false)
+    val context = app.run(*args)
+    Runtime.getRuntime().addShutdownHook(Thread(KafkaShutdownHook(context, context::close)))
 }
