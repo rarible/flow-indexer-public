12d63f8b5d740243fe6b6c183b73bb0a25a5fa8f
Dmitry Saraykin <31763675+dmitry-saraykin@users.noreply.github.com>
2023-09-13 14:59:41 +0300

    Feature/pt 3630 kafka shutdown hook (#529)
    * feat(PT-3683): enable kafka compression
    
    * feat(PT-3683): enable kafka compression
    
    * feat(PT-3630): kafka shutdown hook
    
    * feat(PT-3630): kafka shutdown hook

--- api/src/main/kotlin/com/rarible/flow/api/FlowMarketplaceBackendApp.kt
+++ api/src/main/kotlin/com/rarible/flow/api/FlowMarketplaceBackendApp.kt
@@ -1,11 +1,16 @@
 package com.rarible.flow.api
 
+import ch.sbb.esta.openshift.gracefullshutdown.GracefulShutdownHook
+import com.rarible.core.kafka.KafkaShutdownHook
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class FlowMarketplaceBackendApp
 
 fun main(args: Array<String>) {
-    runApplication<FlowMarketplaceBackendApp>(*args)
+    val app = SpringApplication(FlowMarketplaceBackendApp::class.java)
+    app.setRegisterShutdownHook(false)
+    val context = app.run(*args)
+    Runtime.getRuntime().addShutdownHook(Thread(KafkaShutdownHook(context, GracefulShutdownHook(context))))
 }
