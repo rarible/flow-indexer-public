7945de0c1ed28194768e91282d14f8a570586ee0
Artem Yakischik <a.yakischik@rarible.com>
2023-10-23 16:32:29 +0400

    gamisodes prepatch

--- listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/meta/HWOtherGarageCardV2MetaSubscriber.kt
+++
@@ -1,7 +0,0 @@
-package com.rarible.flow.scanner.subscriber.meta
-
-// @Component
-// class HWOtherGarageCardV2MetaSubscriber: HWGarageCardV2MetaSubscriber() {
-//    override val name = "hw_other_meta_card_v2"
-//    override val contract = Contracts.HW_OTHER_GARAGE_PM_V2
-// }
