e24da565b5cc97bbb373e9d5e4010f6a2d483ac3
sanufriev <s.anufriev@rarible.com>
2023-11-04 09:11:31 +0700

    feat: Hot fix for Gamisodes collection

--- listener/src/main/kotlin/com/rarible/flow/scanner/activity/nft/GamisodesActivityMaker.kt
+++ listener/src/main/kotlin/com/rarible/flow/scanner/activity/nft/GamisodesActivityMaker.kt
@@ -3,8 +3,12 @@ package com.rarible.flow.scanner.activity.nft
 import com.rarible.blockchain.scanner.flow.repository.FlowLogRepository
 import com.rarible.flow.Contracts
 import com.rarible.flow.core.domain.FlowLogEvent
+import com.rarible.flow.core.domain.FlowLogType
+import com.rarible.flow.core.domain.TransferActivity
 import com.rarible.flow.scanner.TxManager
 import com.rarible.flow.scanner.config.FlowListenerProperties
+import com.rarible.flow.scanner.model.WithdrawEvent
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -17,4 +21,30 @@ class GamisodesActivityMaker(
     override fun meta(logEvent: FlowLogEvent): Map<String, String> = emptyMap()
 
     override val contractName: String = Contracts.GAMISODES.contractName
+
+    override suspend fun getDepositTransferActivity(event: FlowLogEvent): TransferActivity? {
+        val deposit = deposit(event)
+        val events = findBefore(deposit, listOf(FlowLogType.WITHDRAW, FlowLogType.MINT))
+        if (events.size > 1) {
+            logger.info("Found more then 1 event coupled with deposit: tx=${event.log.transactionHash}, index=${event.log.eventIndex}")
+        }
+        val withdraw = when (val nftEvent = events.firstOrNull()) {
+            is WithdrawEvent -> nftEvent
+            null -> null
+            else -> return null
+        }
+        return if (deposit.optionalTo != null) {
+            TransferActivity(
+                contract = deposit.collection,
+                tokenId = deposit.tokenId,
+                to = deposit.to,
+                from = withdraw?.from ?: deposit.contractAddress,
+                timestamp = event.log.timestamp
+            )
+        } else null
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(GamisodesActivityMaker::class.java)
+    }
 }
