fe2bd17531b3879dff5640bf0b141bf45c44f508
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2023-11-07 10:57:10 +0400

    Release/1.69 (#544)
    * Gamisodes subscriber fix (#543)
    
    * Gamisodes subscriber fix
    
    * Gamisodes subscriber fix
    
    * Lint fix
    
    * feat: Hot fix for Gamisodes collection
    
    * feat: Improve log
    
    ---------
    
    Co-authored-by: sanufriev <s.anufriev@rarible.com>

--- listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/nft/GamisodesTokenSubscriber.kt
+++ listener/src/main/kotlin/com/rarible/flow/scanner/subscriber/nft/GamisodesTokenSubscriber.kt
@@ -1,6 +1,7 @@
 package com.rarible.flow.scanner.subscriber.nft
 
 import com.rarible.flow.Contracts
+import com.rarible.flow.scanner.model.NonFungibleTokenEventType
 import com.rarible.flow.scanner.subscriber.EnableGamisodesToken
 import org.springframework.stereotype.Component
 
@@ -8,6 +9,22 @@ import org.springframework.stereotype.Component
 @EnableGamisodesToken
 class GamisodesTokenSubscriber : NonFungibleTokenSubscriber() {
 
+    private val mintEventName = "NFTMinted"
+    private val burnEventName = "NFTBurned"
+
+    override val events = setOf(
+        mintEventName,
+        NonFungibleTokenEventType.DEPOSIT.eventName,
+        NonFungibleTokenEventType.WITHDRAW.eventName,
+        burnEventName
+    )
     override val name = "gamisodes"
     override val contract = Contracts.GAMISODES
+
+    override fun fromEventName(eventName: String) =
+        when (eventName) {
+            mintEventName -> NonFungibleTokenEventType.MINT
+            burnEventName -> NonFungibleTokenEventType.BURN
+            else -> super.fromEventName(eventName)
+        }
 }
