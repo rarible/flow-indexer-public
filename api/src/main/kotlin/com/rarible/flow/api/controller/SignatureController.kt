5d091a3a352511f63163b65cd6277fd56a03d93f
Artem Yakischik <a.yakischik@rarible.com>
2023-11-20 19:00:55 +0400

    fix: Signature algorithm param query added

--- api/src/main/kotlin/com/rarible/flow/api/controller/SignatureController.kt
+++ api/src/main/kotlin/com/rarible/flow/api/controller/SignatureController.kt
@@ -2,9 +2,11 @@ package com.rarible.flow.api.controller
 
 import com.nftco.flow.sdk.FlowPublicKey
 import com.nftco.flow.sdk.FlowSignature
+import com.nftco.flow.sdk.SignatureAlgorithm
 import com.rarible.flow.api.service.SignatureService
 import com.rarible.flow.api.util.flowAddress
 import com.rarible.flow.api.util.okOr404IfNull
+import com.rarible.protocol.dto.FlowSignatureAlgorithmDto
 import com.rarible.protocol.flow.nft.api.controller.FlowNftCryptoControllerApi
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -24,13 +26,22 @@ class SignatureController(
         publicKey: String,
         signerAddress: String,
         signature: String,
-        message: String
+        message: String,
+        algorithm: FlowSignatureAlgorithmDto?
     ): ResponseEntity<Boolean> {
         val pk = FlowPublicKey(publicKey)
         val sig = FlowSignature(signature)
+        val algo = when (algorithm) {
+            FlowSignatureAlgorithmDto.ECDSA_secp56k1 -> SignatureAlgorithm.ECDSA_SECP256k1
+            FlowSignatureAlgorithmDto.ECDSA_P256 -> SignatureAlgorithm.ECDSA_P256
+            else -> SignatureAlgorithm.ECDSA_SECP256k1 // Default
+        }
         val result = try {
             val sigCheck = signatureService.verify(
-                pk, sig, message
+                publicKey = pk,
+                signature = sig,
+                message = message,
+                algorithm = algo
             )
             val accountCheck = signatureService.checkPublicKey(
                 signerAddress.flowAddress()!!,
