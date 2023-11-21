5f4132fdcc4b4fbec982af5c76c213ad0633fe7b
Roman Novikov <mystdeim@gmail.com>
2023-11-21 23:14:13 +0000

    fix: add createdAt for cache

--- core/src/main/kotlin/com/rarible/flow/core/domain/RawOnChainMeta.kt
+++ core/src/main/kotlin/com/rarible/flow/core/domain/RawOnChainMeta.kt
@@ -1,14 +1,19 @@
 package com.rarible.flow.core.domain
 
+import com.rarible.core.common.nowMillis
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 import org.springframework.data.mongodb.core.mapping.FieldType
 import org.springframework.data.mongodb.core.mapping.MongoId
+import java.time.Instant
 
 @Document(RawOnChainMeta.COLLECTION)
 data class RawOnChainMeta(
     @MongoId(FieldType.STRING)
     val id: String,
-    val data: String
+    val data: String,
+    @Field(targetType = FieldType.DATE_TIME)
+    val createdAt: Instant? = nowMillis()
 ) {
 
     companion object {
